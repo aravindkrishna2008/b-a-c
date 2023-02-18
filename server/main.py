40e160a04a2bda252cf309e07fe83181a99df6c0
Veerrohit Veeravadivel <veerrohitv08@gmail.com>
2023-02-18 10:28:21 -0800

    idk

---
+++ server/main.py
@@ -0,0 +1,16 @@
+# import whisper
+#
+# model = whisper.load_model("base")
+# audio = whisper.load_audio("hello.mp3")
+#
+# mel = whisper.log_mel_spectrogram(audio).to(model.device)
+#
+# _, probs = model.detect_language(mel)
+# print(f"Detected language: {max(probs, key=probs.get)}")
+#
+# options = whisper.DecodingOptions()
+# result = whisper.decode(model, mel, options)
+#
+# print(result.text)
+
+print("HI")
