4788be4a2bdb253c642b0d74e2d67601ee18029b
aravindkrishna2008 <aravindkrishna.nambi@gmail.com>
2023-02-18 21:58:07 -0800

    basic features working

--- server/server.py
+++ server/server.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
+import whisper
+model = whisper.load_model("base")
+
 
 app = Flask(__name__)
 CORS(app)
@@ -17,7 +20,23 @@ def uploadFile():
         file = request.files['file']
         print("File taken")
         file.save(os.path.join(app.config["IMAGE_UPLOADS"], file.filename))
-        return jsonify("File uploaded")
+        audio = whisper.load_audio(file.filename)
+        audio = whisper.pad_or_trim(audio)
+
+        # make log-Mel spectrogram and move to the same device as the model
+        mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+        # detect the spoken language
+        _, probs = model.detect_language(mel)
+        print(f"Detected language: {max(probs, key=probs.get)}")
+
+        # decode the audio
+        options = whisper.DecodingOptions(fp16=False)
+        result = whisper.decode(model, mel, options)
+
+        # print the recognized text
+        print(result.text)
+        return jsonify(result.text)
         
 
 app.run(host='0.0.0.0', port="5001", debug=True)
