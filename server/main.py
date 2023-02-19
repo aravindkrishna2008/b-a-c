f5e2c3591a4a348ea6586097dff926d4403e708a
aravindkrishna2008 <aravindkrishna.nambi@gmail.com>
2023-02-18 20:19:51 -0800

    stuff

--- server/main.py
+++ server/main.py
@@ -1,14 +1,21 @@
 import whisper
 
 model = whisper.load_model("base")
-audio = whisper.load_audio("hello.mp3")
 
+# load audio and pad/trim it to fit 30 seconds
+audio = whisper.load_audio("hello.mp3")
+audio = whisper.pad_or_trim(audio)
+
+# make log-Mel spectrogram and move to the same device as the model
 mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
+# detect the spoken language
 _, probs = model.detect_language(mel)
 print(f"Detected language: {max(probs, key=probs.get)}")
 
-options = whisper.DecodingOptions()
+# decode the audio
+options = whisper.DecodingOptions(fp16=False)
 result = whisper.decode(model, mel, options)
 
+# print the recognized text
 print(result.text)
