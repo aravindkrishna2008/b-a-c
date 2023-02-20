8e1774dfc8dab982bc90955f3f728db04b8d3a89
aravindkrishna2008 <aravindkrishna.nambi@gmail.com>
2023-02-19 18:38:14 -0800

    stuff

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,401 +1,103 @@
-import MicRecorder from "mic-recorder-to-mp3";
-import axios from "axios";
+import { useEffect, useRef } from "react";
 
-const Mp3Recorder = new MicRecorder({ bitRate: 128 });
+import { useRouter } from "next/router";
 
-import Image from "next/image";
+export default function MyComponent() {
+  const blobRef = useRef(null);
 
-// Import alphabet images
-
-import { useEffect, useState } from "react";
-
-const App = () => {
-  const [isRecording, setIsRecording] = useState(false);
-  const [blobURL, setBlobURL] = useState("");
-  const [isBlocked, setIsBlocked] = useState(false);
-
-  const [text, setText] = useState([]);
-  const [alphabet, setAlphabet] = useState([]);
-
-  const [file, setFile] = useState(null);
-  const [data, setData] = useState("");
+  const router = useRouter();
 
   useEffect(() => {
-    setAlphabet(data.split(""));
-    history();
-  }, [data]);
+    const blob = blobRef.current;
+    if (!blob) return;
 
-  const start = () => {
-    if (isBlocked) {
-      console.log("Permission Denied");
-    } else {
-      setData("");
-      Mp3Recorder.start()
-        .then(() => {
-          setIsRecording(true);
-        })
-        .catch((e) => console.error(e));
-    }
-  };
+    // Glow effect
+    const handlePointerMove = (event) => {
+      const { clientX, clientY } = event;
 
-  // create function to send file to server using form data
-  const sendFile = async (file) => {
-    const formData = new FormData();
-    formData.append("file", file);
-    try {
-      const res = await axios.post(
-        "http://localhost:5001/upload-file/",
-        formData,
+      blob.animate(
         {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
+          left: `${clientX}px`,
+          top: `${clientY}px`,
+        },
+        { duration: 3000, fill: "forwards" }
       );
-      // const { fileName, filePath } = res.data;
-      // setFile({ fileName, filePath });
-      setData(res.data);
-      console.log(res.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+    };
 
-  const history = () => {
-    // get all the past text from the server using fetch
-    fetch("http://localhost:5001/translations/")
-      .then((response) => response.json())
-      .then((data) => {
-        // setText(data);
-        console.log(data);
-        setText(data.map((item) => item.text));
-      })
-      .catch((error) => console.error(error));
-  };
+    window.addEventListener("pointermove", handlePointerMove);
 
-  const stop = () => {
-    Mp3Recorder.stop()
-      .getMp3()
-      .then(([buffer, blob]) => {
-        const blobURL = URL.createObjectURL(blob);
-        // save the mp3 file
-        const file = new File(buffer, "me-at-thevoice.mp3", {
-          type: blob.type,
-          lastModified: Date.now(),
-        });
+    return () => {
+      window.removeEventListener("pointermove", handlePointerMove);
+    };
+  }, [blobRef]);
 
-        const player = new Audio(URL.createObjectURL(file));
-        // send file to server
-        sendFile(file);
-        console.log(player);
-        setBlobURL(blobURL);
-        setIsRecording(false);
-        console.log(blobURL);
-      })
-      .catch((e) => console.log(e));
-    // save the mp3 file
-  };
+  useEffect(() => {
+    // Text effect
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let interval = null;
 
-  const renderTextArray = () => {
-    return text.map((text) => {
-      return <div className="history-item">{text.substring(0, 30)}</div>;
-    });
-  };
+    const handleMouseOver = (event) => {
+      let iteration = 0;
+
+      clearInterval(interval);
+
+      interval = setInterval(() => {
+        event.target.innerText = event.target.innerText
+          .split("")
+          .map((letter, index) => {
+            if (index < iteration) {
+              return event.target.dataset.value[index];
+            }
+
+            return letters[Math.floor(Math.random() * 26)];
+          })
+          .join("");
+
+        if (iteration >= event.target.dataset.value.length) {
+          clearInterval(interval);
+        }
+
+        iteration += 1 / 3;
+      }, 30);
+    };
+
+    const h1 = document.querySelector("h1");
+    h1.addEventListener("mouseover", handleMouseOver);
+
+    return () => {
+      h1.removeEventListener("mouseover", handleMouseOver);
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <div className="app">
-      <div className="app-header">
-        <div className="controls">
-          <button className="start-btn" onClick={start} disabled={isRecording}>
-            Record
+    <div>
+      <div id="blob" ref={blobRef}></div>
+      <div id="blur"></div>
+      <h1 data-value="TALK2HANDS">TALK2HANDS</h1>
+      <button></button>
+      <div id="links">
+        <div className="meta-link">
+          <button
+            onClick={() => {
+              router.push("/about");
+            }}
+            data-value="TALK2HANDS"
+            className="glow-on-hover"
+          >
+            <p className="text-in-button">About</p>
           </button>
-          <button className="stop_btn" onClick={stop} disabled={!isRecording}>
-            Stop
+          <button
+            onClick={() => {
+              router.push("/demo");
+            }}
+            data-value="TALK2HANDS"
+            className="glow-on-hover"
+          >
+            <p className="text-in-button">Demo</p>
           </button>
-          {isRecording ? (
-            <div className="blink_me"></div>
-          ) : (
-            <div>Waiting to start...</div>
-          )}{" "}
         </div>
-        <div>{renderTextArray()}</div>
-      </div>
-      <div className="sign-language">
-        {alphabet.map((letter) => {
-          if (letter.toLowerCase() === "a") {
-            return (
-              <img
-                src="/alphabet/a.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "b") {
-            return (
-              <Image
-                src="/alphabet/b.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "c") {
-            return (
-              <Image
-                src="/alphabet/c.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "d") {
-            return (
-              <Image
-                src="/alphabet/d.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "e") {
-            return (
-              <Image
-                src="/alphabet/e.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "f") {
-            return (
-              <Image
-                src="/alphabet/f.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "g") {
-            return (
-              <Image
-                src="/alphabet/g.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "h") {
-            return (
-              <Image
-                src="/alphabet/h.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "i") {
-            return (
-              <Image
-                src="/alphabet/i.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "j") {
-            return (
-              <Image
-                src="/alphabet/j.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "k") {
-            return (
-              <Image
-                src="/alphabet/k.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "l") {
-            return (
-              <Image
-                src="/alphabet/l.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "m") {
-            return (
-              <Image
-                src="/alphabet/m.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "n") {
-            return (
-              <Image
-                src="/alphabet/n.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "o") {
-            return (
-              <Image
-                src="/alphabet/o.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "p") {
-            return (
-              <Image
-                src="/alphabet/p.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "q") {
-            return (
-              <Image
-                src="/alphabet/q.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "r") {
-            return (
-              <Image
-                src="/alphabet/r.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "s") {
-            return (
-              <Image
-                src="/alphabet/s.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "t") {
-            return (
-              <Image
-                src="/alphabet/t.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "u") {
-            return (
-              <Image
-                src="/alphabet/u.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "v") {
-            return (
-              <Image
-                src="/alphabet/v.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "w") {
-            return (
-              <Image
-                src="/alphabet/w.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "x") {
-            return (
-              <Image
-                src="/alphabet/x.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "y") {
-            return (
-              <Image
-                src="/alphabet/y.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === "z") {
-            return (
-              <Image
-                src="/alphabet/z.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          } else if (letter.toLowerCase() === " ") {
-            return (
-              <Image
-                src="/alphabet/space.jpg"
-                alt="logo"
-                className="asl-image"
-                width={130}
-                height={200}
-              />
-            );
-          }
-        })}
       </div>
     </div>
   );
-};
-
-export default App;
+}
--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import AudioRecorderPlayer, {
+  AVEncoderAudioQualityIOSType,
+  AVEncodingOption,
+  AudioEncoderAndroidType,
+  AudioSet,
+  AudioSourceAndroidType,
+} from "react-native-audio-recorder-player";
+import { Card } from "react-native-paper";
+import React from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const audioRecorderPlayer = new AudioRecorderPlayer();
+  const [recordSecs, setRecordSecs] = React.useState(0);
+  const [recordTime, setRecordTime] = React.useState(0);
+  const [currentPositionSec, setCurrentPositionSec] = React.useState(0);
+  const [currentDurationSec, setCurrentDurationSec] = React.useState(0);
+  const [playTime, setPlayTime] = React.useState(0);
+  const [duration, setDuration] = React.useState(0);
+
+  const onStartRecord = async () => {
+    const result = await audioRecorderPlayer.startRecorder();
+    audioRecorderPlayer.addRecordBackListener((e) => {
+      setRecordSecs(e.current_position);
+      setRecordTime(audioRecorderPlayer.mmssss(Math.floor(e.current_position)));
+      return;
+    });
+    console.log(result);
+  };
+
+  const onStopRecord = async () => {
+    const result = await audioRecorderPlayer.stopRecorder();
+    audioRecorderPlayer.removeRecordBackListener();
+    setRecordSecs(0);
+    console.log(result);
+  };
+
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
